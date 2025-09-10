495c77fa96c2f1d0dd798cc4a3b6987ead604025
tyler-radke1 <109247761+tyler-radke1@users.noreply.github.com>
2025-09-09 20:23:28 -0600

    Week 1 work. Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = "Last Modified: " + lastModified;
